6b382239f8789deccbf062cdf6f89c39c300b5da
Yan Xu <xudaiyanzi@gmail.com>
2021-08-31 11:13:40 -0500

    reorganize

---
+++ test-APP/src/MyFirstApp.java
@@ -0,0 +1,11 @@
+public class MyFirstApp {
+//    the string should have '[]'
+    public static void main (String[] args) {
+//        the print is different from println
+        System.out.print("My 1st Java sentence!");
+        System.out.print("My 2nd Java sentence!");
+        System.out.println("My 3r Java sentence!");
+        System.out.println("My 4th Java sentence!");
+    }
+}
+
